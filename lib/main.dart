a634eeb583687973171e839fbdfd03e15894128c
PC-39 Bisnis Digital <pc-39bisnisdigital@PC-39s-iMac.local>
2023-10-12 10:35:43 +0700

    commit project 1-1

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: "BINA SUKSES JEJAMA",
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.lightBlue,
+          title: Text("BINA SUKSES JEJAMA"),
+        ),
+  
+      ),
+    );
+  }
+}
+
